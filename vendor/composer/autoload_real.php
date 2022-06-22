f3c569f3a5a47fac10b32a8ac5ccdfaa6e11de8e
marliisros <marliis17@gmail.com>
2022-06-22 12:01:48 +0300

    Initial commit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit839fc523b8fcaab05d6eb53a503bd690
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit839fc523b8fcaab05d6eb53a503bd690', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit839fc523b8fcaab05d6eb53a503bd690', 'loadClassLoader'));
